27ea686c89da708311163baa709b7e0c14bb14ea
Stefan Härtel <haertel.dev@gmail.com>
2019-03-04 00:48:09 +0100

    Minor refactoring

--- main_frame.py
+++ main_frame.py
@@ -4,5 +4,4 @@ import wx.adv
 class MainFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         super(MainFrame, self).__init__(*args, **kwargs)
-        self.tb_icon = wx.adv.TaskBarIcon()
-        print(self.tb_icon.IsOk())
+        pass
--- sshortcut.py
+++ sshortcut.py
@@ -1,11 +1,20 @@
 import wx
 from main_frame import MainFrame
 
+def create_icon():
+    if not wx.adv.TaskBarIcon.IsAvailable():
+        print("The taskbar icon is not available!") #TODO Handle this.
+    icon = wx.adv.TaskBarIcon()
+
 def create_frame():
     frame = MainFrame(None, title="SSHortcut")
     frame.Show()
 
-if __name__ == '__main__':
+def initialize():
     application = wx.App()
+    create_icon()
     create_frame()
     application.MainLoop()
+
+if __name__ == '__main__':
+    initialize()
