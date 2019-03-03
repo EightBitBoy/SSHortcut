23b6a08edfe5b9c655f5627dac3c146b59e84b77
Stefan Härtel <haertel.dev@gmail.com>
2019-03-04 00:37:52 +0100

    Execute module

--- sshortcut.py
+++ sshortcut.py
@@ -5,6 +5,7 @@ def create_frame():
     frame = MainFrame(None, title="SSHortcut")
     frame.Show()
 
-application = wx.App()
-create_frame()
-application.MainLoop()
+if __name__ == '__main__':
+    application = wx.App()
+    create_frame()
+    application.MainLoop()
