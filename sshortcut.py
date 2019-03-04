a35865993df9a7ab6d1a7b6bcee5096a53371847
Stefan Härtel <haertel.dev@gmail.com>
2019-03-04 01:07:16 +0100

    Try setting graphics

--- sshortcut.py
+++ sshortcut.py
@@ -5,6 +5,12 @@ def create_icon():
     if not wx.adv.TaskBarIcon.IsAvailable():
         print("The taskbar icon is not available!") #TODO Handle this.
     icon = wx.adv.TaskBarIcon()
+    graphics = wx.Icon()
+    loadSuccessful = graphics.LoadFile("icon_tray.png", type=wx.BITMAP_TYPE_PNG)
+    print(loadSuccessful) #TODO Do something with this.
+    setIconSuccessful = icon.SetIcon(graphics)
+    print(setIconSuccessful) #TODO Do something with this.
+    print(icon.IsIconInstalled()) #TODO Do something with this.
 
 def create_frame():
     frame = MainFrame(None, title="SSHortcut")
